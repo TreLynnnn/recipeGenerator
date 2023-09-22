0c7916a6d5bb99e79a67a06f38f6611233922b43
TreLyn <trelyn.dev@gmail.com>
2023-09-22 05:16:25 +0000

    refactor file structure

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-const { MongoClient } = require('mongodb');
+const express = require('express');
+const path = require('path');
+
+// Create an Express application
+const app = express();
+
+// Define a port for your server to listen on
+const port = process.env.PORT || 3000;
+
+// Middleware for serving static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes for different pages
+app.get('/', (req, res) => {
+  // Serve the homepage (index.html)
+  res.sendFile(path.join(__dirname, 'public', '/views/index.html'));
+});
+
+// app.get('/about', (req, res) => {
+//   // Serve the "about" page (about.html)
+//   res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+// app.get('/contact', (req, res) => {
+//   // Serve the "contact" page (contact.html)
+//   res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+// });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
